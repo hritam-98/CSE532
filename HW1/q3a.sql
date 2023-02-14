da4965dd9cef0c845e78da8d259dd48cd86366ad
Hritam Basak <hritambasak48@gmail.com>
2023-02-14 17:16:45 -0500

    Add files via upload

---
+++ HW1/q3a.sql
@@ -0,0 +1,13 @@
+select year(transaction_date) || 
+case 
+when (cast (month(transaction_date) as int) < 10)
+then '0' || cast(month(transaction_date) as int)
+else cast (month(transaction_date) as char(2))
+end as yeardate,
+BUYER_COUNTY AS county, 
+decimal(sum(dosage_unit),31,2) as monthly_counts,
+decimal(avg(sum(dosage_unit)) 
+	over (order by year(transaction_date), month(transaction_date) rows between 1 PRECEDING and 1 following ),31,2) as smooth_counts
+from cse532.dea_ny WHERE BUYER_COUNTY = 'SUFFOLK'
+group by year(transaction_date), month(transaction_date),BUYER_COUNTY 
+
---
+++ HW1/zipload.sql
@@ -0,0 +1,1 @@
+load from "C:/zip_code_database.csv" of DEL method P(1,7,8,15) REPLACE INTO CSE532.ZIP(ZIP,STATE,COUNTY,POP);
